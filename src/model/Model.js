e99d036883632a9b37108d1b41dbd21e8abfa688
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-06 13:41:17 +0530

    add model and metadata file

---
+++ src/model/Model.js
@@ -0,0 +1,6 @@
+export const ASL_MODEL = {
+  name: "tm-my-image-model",
+  classes: ["default", "close", "yawn", "headfall", "eyerub"],
+  modelJson: "http://localhost:3000/model.json",
+  metadataJson: "http://localhost:3000/metadata.json",
+};
