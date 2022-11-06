058554377607fa3317d6e4bdf89a633cdd412b81
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-06 14:12:20 +0530

    use webcam component

--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useModel } from "../hooks/useModel";
 import { ASL_MODEL } from "../model/Model";
+import Webcam from "./Webcam";
+import { Text } from "@chakra-ui/react";
 
 const Prediction = ({ isRecording, predictedFeature, setPredictedFeature }) => {
   const { isModelLoaded, predictClass } = useModel(ASL_MODEL);
@@ -25,7 +27,21 @@ const Prediction = ({ isRecording, predictedFeature, setPredictedFeature }) => {
     [isModelLoaded, predictClass]
   );
 
-  return <div>Prediction</div>;
+  return (
+    <>
+      <Webcam
+        canvas={{ width: 200, height: 200 }}
+        updateInterval={100}
+        isRecording={isRecording}
+        onUpdate={onUpdate}
+      />
+      <Text fontSize="sm" color={"gray.500"}>
+        {isRecording
+          ? `Predicted: ${predictedClass}`
+          : "Press the Record button to start prediction"}
+      </Text>
+    </>
+  );
 };
 
 export default Prediction;
