8aad1e16ea7e22fa082b70d9861ab0d7a44a323f
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-06 14:27:02 +0530

    implement record feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Prediction from "./components/Prediction";
 import Header from "./components/Header";
+import { Button, VStack } from "@chakra-ui/react";
 
 function App() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [predictedFeature, setPredictedFeature] = useState("");
+  const [predictions, setPredictions] = useState([""]);
+
+  useEffect(() => {
+    if (isRecording) {
+      setPredictions((oldArray) => [...oldArray, predictedFeature]);
+    } else {
+      setPredictions([""]);
+    }
+  }, [isRecording, predictedFeature]);
+
+  console.log("predictions", predictions);
+
   return (
     <div className="App">
       <Header />
-      <Prediction />
+      <VStack>
+        <Prediction
+          isRecording={isRecording}
+          predictedFeature={predictedFeature}
+          setPredictedFeature={setPredictedFeature}
+        />
+        <Button
+          colorScheme="red"
+          variant={"outline"}
+          isLoading={isRecording}
+          loadingText="Recording"
+          onClick={() => setIsRecording(!isRecording)}
+        >
+          Record
+        </Button>
+      </VStack>
     </div>
   );
 }
